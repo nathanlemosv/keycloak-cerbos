d75272ac52597b613013d65aa631667e16b2c25c
Nathan Lemos <nathanlemos@MacBook-Pro-do-Nathan.local>
2025-05-12 09:58:11 -0300

    started resource mock api

---
+++ api/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	logger := log.New(os.Stdout, "resource-api: ", log.LstdFlags)
+	data, err := os.ReadFile("./data/response.payload.json")
+	check(logger, err)
+	var resourceList ResourceList
+	err = json.Unmarshal(data, &resourceList)
+	check(logger, err)
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, resourceList)
+	})
+	err = r.Run(":8585")
+	check(logger, err)
+}
+
+type ResourceList struct {
+	Resources []Resource `json:"resources"`
+}
+type Resource struct {
+	Description string `json:"description"`
+}
+
+func check(logger *log.Logger, e error) {
+	if e != nil {
+		logger.Fatal(e)
+	}
+}
